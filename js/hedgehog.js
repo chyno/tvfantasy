9d061ab96593d804747570fd9bb95195440757fa
chyno <jchynowe1@yahoo.com>
2019-08-22 18:12:53 -0400

    add es file

--- js/hedgehog.js
+++ js/hedgehog.js
@@ -8,4 +8,6 @@ firebaseService.createIfNotExists(AUTH_TABLE, obj.lookupKey, obj);
 const setUserFn = async obj =>
 firebaseService.createIfNotExists(USER_TABLE, obj.username, obj);
 const getFn = async obj => firebaseService.readRecordFromFirebase(AUTH_TABLE, obj);
+
+
 export const hedgehog = new Hedgehog(getFn, setAuthFn, setUserFn);
