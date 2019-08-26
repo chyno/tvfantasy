4ee10677740a8dc108bbbab00a81e9457690f818
chyno <jchynowe1@yahoo.com>
2019-08-26 16:13:14 -0400

    fauna

--- js/fauna.js
+++ js/fauna.js
@@ -1,41 +1,21 @@
 const secretAdminKey = "fnADWu_uwLACCI7LXiCJ7Szqvqjvk8BFndUFRMvy";
 
 readAuthRecordFromDb = async (tvlistingsClient, tvlistingsQuery, obj) => {
-  const userIndex = 'users_by_username';
-  if(!obj || !obj.lookupKey) {
-    throw('Valid data not passed in');
-  }
-  let lookupKey = obj.lookupKey;
-
+                                
+  const authenticationsIndex = 'authentications_by_lookupkey';
+  
   try {
     let ret = await tvlistingsClient.query(
       tvlistingsQuery.Get(
-        tvlistingsQuery.Match(tvlistingsQuery.Index(userIndex), lookupKey)));
+        tvlistingsQuery.Match(tvlistingsQuery.Index(authenticationsIndex), obj.lookupKey)));
      console.log(ret);
      if (ret && ret.data)  {
        return ret.data;
-     } else {throw('can not get data');}
+     }
 
    } catch (e) {
      throw(e);
- }
-};
-
-createIfNotExists = async (tvlistingsClient, tvlistingsQuery,collection, obj) => {
-    // Todo: Check is exists
-  
-    try {
-      let response = await tvlistingsClient.query(
-      tvlistingsQuery.Create(tvlistingsQuery.Collection(collection), { data: obj }));
-      return response.ref;
-    } catch (e) {
-        console.log('**** Error :' +e);
-        throw e;
-    } 
-  };
-
-
-module.exports = {
-  readAuthRecordFromDb,
+   }
+adAuthRecordFromDb,
   createIfNotExists
 };
--- test/fauna-tests.js
+++ test/fauna-tests.js
@@ -1,143 +1,198 @@
 var assert = require("assert");
-var faunadb = require('faunadb'),
+var faunadb = require("faunadb"),
   q = faunadb.query;
-  const userIndex = 'users_by_username';
-  const secretAdminKey = "fnADWu_uwLACCI7LXiCJ7Szqvqjvk8BFndUFRMvy";
- // const secretClientKey = 'fnADWgKmE-ACCNt8DvTuqmsjsRC71C3AcoGbPJ7x';
+const userIndex = "users_by_username";
+const authenticationsIndex = "authentications_by_lookupkey";
+
+const secretAdminKey = "fnADWu_uwLACCI7LXiCJ7Szqvqjvk8BFndUFRMvy";
+// const secretClientKey = 'fnADWgKmE-ACCNt8DvTuqmsjsRC71C3AcoGbPJ7x';
 //var client = new faunadb.Client({ secret: 'YOUR_FAUNADB_SECRET' });
 
-var fauna  =  require('../JS/fauna.js');
+var fauna = require("../JS/fauna.js");
 
 xdescribe("Raw testing of Fauna Javascript API", function() {
-    let client = null;
+  let client = null;
 
-    beforeEach(async function() {
-      client = new faunadb.Client({ secret: secretAdminKey });
-    });
-  
-    it("Can create client", function() {
-      assert.ok(client, "fauna client created");
-    });
-    
-    
+  beforeEach(async function() {
+    client = new faunadb.Client({ secret: secretAdminKey });
+  });
 
-    xit('Can Create an index on the users’s username:', async function() {
-      let item = q.CreateIndex(
-        { name: userIndex,
-          source: q.Collection("users"),
-          terms: [{ field: ["data", "username"] }] });
-      try {
+  xit("Can create client", function() {
+    assert.ok(client, "fauna client created");
+  });
+
+  xit("Can Create an index on the authorization’s username:", async function() {
+    let item = q.CreateIndex({
+      name: authenticationsIndex,
+      source: q.Collection("authentications"),
+      terms: [{ field: ["data", "authentication"] }]
+    });
+    try {
       let ret = await client.query(item);
       console.log(ret);
-      } catch (e) {
-        const resp = e.requestResult.responseRaw;
-        console.log(resp);
-        assert.fail(e,resp);
+    } catch (e) {
+      const resp = e.requestResult.responseRaw;
+      console.log(resp);
+      assert.fail(e, resp);
     }
-      //.then((ret) => console.log(ret)
-      
+    //.then((ret) => console.log(ret)
+  });
+
+  xit("Can Create an index on the users’s username:", async function() {
+    let item = q.CreateIndex({
+      name: userIndex,
+      source: q.Collection("users"),
+      terms: [{ field: ["data", "username"] }]
+    });
+    try {
+      let ret = await client.query(item);
+      console.log(ret);
+    } catch (e) {
+      const resp = e.requestResult.responseRaw;
+      console.log(resp);
+      assert.fail(e, resp);
+    }
+    //.then((ret) => console.log(ret)
+  });
+
+  xit("can add record", async function() {
+    let item = q.Create(q.Collection("users"), {
+      data: { username: "myusername", foo: "bar" }
     });
 
-    xit('can add record', async function() {
-      let item = q.Create(
-        q.Collection("users"),
-        { data: { username: "myusername", foo: 'bar' } });
+    assert(item);
+    try {
+      let ret = await client.query(item);
+      console.log(ret);
+      assert(ret);
+    } catch (e) {
+      const resp = e.requestResult.responseRaw;
+      console.log(resp);
+      assert.fail(e, resp);
+    }
+  });
 
-        assert(item);
-        try {
-          let ret = await client.query(item);
-          console.log(ret);
-          assert(ret);
-        } catch (e) {
-          const resp = e.requestResult.responseRaw;
-          console.log(resp);
-          assert.fail(e,resp);
-      }
-    });
+  xit("can get user record by id ", async function() {
+    // assert(item);
+    try {
+      let ret = await client.query(
+        q.Get(q.Ref(q.Collection("users"), "241771288921637384"))
+      );
 
-    xit('can get user record by id ', async function() {
-    
-       // assert(item);
-        try {
-          let ret = await client.query(q.Get(q.Ref(q.Collection("users"), "241771288921637384")));
+      //.then((ret) => console.log(ret));
+      console.log(ret);
+      assert(ret);
 
-          //.then((ret) => console.log(ret));
-          console.log(ret);
-          assert(ret);
+      // get data
+      let data = ret.data;
+      console.log(data);
+      assert(data);
 
-          // get data
-          let data = ret.data;
-          console.log(data);
-          assert(data);
+      // get data
+      let username = data.username;
+      console.log(username);
+      assert(username);
+    } catch (e) {
+      const resp = e.requestResult.responseRaw;
+      console.log(resp);
+      assert.fail(e, resp);
+    }
+  });
 
-          // get data
-          let username = data.username;
-          console.log(username);
-          assert(username);
-        } catch (e) {
-          const resp = e.requestResult.responseRaw;
-          console.log(resp);
-          assert.fail(e,resp);
-      }
-    });
+  xit("can get user record by username ", async function() {
+    const userName = "myusername";
+    // assert(item);
+    try {
+      let ret = await client.query(
+        q.Get(q.Match(q.Index(userIndex), userName))
+      );
+      // .then((ret) => console.log(ret))
 
-    it('can get user record by username ', async function() {
-      const userName = 'myusername';
-      // assert(item);
-       try {
-        let ret = await client.query(
-          q.Get(
-            q.Match(q.Index(userIndex), userName)));
-       // .then((ret) => console.log(ret))
-        
-         //.then((ret) => console.log(ret));
-         console.log(ret);
-         assert(ret);
+      //.then((ret) => console.log(ret));
+      console.log(ret);
+      assert(ret);
 
-         // get data
-         let data = ret.data;
-         console.log(data);
-         assert(data);
+      // get data
+      let data = ret.data;
+      console.log(data);
+      assert(data);
 
-         // get data
-         let username = data.username;
-         console.log(username);
-         assert(username);
-       } catch (e) {
-         const resp = e.requestResult.responseRaw;
-         console.log(resp);
-         assert.fail(e,resp);
-     }
-   });
+      // get data
+      let username = data.username;
+      console.log(username);
+      assert(username);
+    } catch (e) {
+      const resp = e.requestResult.responseRaw;
+      console.log(resp);
+      assert.fail(e, resp);
+    }
+  });
 });
 
 describe("Test Fauna Service", function() {
   let client = null;
-  
+
   beforeEach(async function() {
     this.client = new faunadb.Client({ secret: secretAdminKey });
-    
   });
 
   xit("fauna class exports functions", function() {
-   
     let fn = fauna.readAuthRecordFromDb;
     assert.ok(fn, "fauna function created");
 
     let fn2 = fauna.createIfNotExists;
     assert.ok(fn2, "fauna function created");
-
   });
 
-  it("fauna function can read  existing record", async function() {
-   
+  xit("fauna function can read  existing record", async function() {
     let fn = fauna.readAuthRecordFromDb;
     assert.ok(fn, "fauna function created");
 
-    let result = await fn(this.client,q,{lookupKey : 'myusername'});
+    let result = await fn(this.client, q, { lookupKey: "myusername" });
     assert.ok(result, "fauna function created");
-
   });
 
-  
+  it("fauna function add new auth", async function() {
+    const collec = "authentications";
+    const lookupKey = "mylookup";
+    const objeTOAdd = {
+      lookupKey: lookupKey,
+      authfoo: "ome other raNDOME DATAs",
+      authbar: "ya ya"
+    };
+
+    // Make sure it is removed
+    //username
+    let ret;
+
+    try {
+      ret = await this.client.query(
+        q.Get(q.Match(q.Index(authenticationsIndex), lookupKey))
+      );
+      var delRes = await this.client.query(
+        q.Delete(q.Ref(q.Collection(collec), ret.ref.id))
+      );
+      assert.ok(delRes);
+    } catch (e) {}
+    
+    let result = null;
+    try {
+      let adddata = await fauna.createIfNotExists(
+        this.client,
+        q,
+        collec,
+        objeTOAdd
+      );
+      assert.ok(adddata, "fauna function created");
+
+      // Now get this record from db
+      let fn2 = fauna.readAuthRecordFromDb;
+      result = await fn2(this.client, q, { lookupKey: lookupKey });
+    } catch (e) {
+      assert.fail(e);
+    }
+    assert.ok(result, "fauna function created");
+    //assert.equal(objeTOAdd.lookupKey, result.lookupKey, 'lookup key should equal');
+    assert.equal(objeTOAdd.foo, result.foo, "lookup key should equal");
+  });
 });
